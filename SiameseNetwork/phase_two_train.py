a4881341a7bb7f94bee9e492f7320d082631e846
GuyShimony <73709874+GuyShimony@users.noreply.github.com>
2022-04-30 16:52:52 +0300

    remove unnecessary comments

--- SiameseNetwork/phase_two_train.py
+++ SiameseNetwork/phase_two_train.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import random
 
-
 '''
-Phase to add Negative Examples from methods of name representation as vectors (can switch with larger file later)
+Phase 2 adding Negative Examples from methods of name representation as vectors
 '''
 
 
 def negative(originals, index):
+    """
+    Create random negative example for the original name at 'index'
+    """
     neg = "None"
     # Each name might be in the dataset 10 times in a row (at most)
     if index <= 9:
@@ -34,11 +36,11 @@ def second_phase_negative_examples(short_file_path, dataset, filtered_df):
     pyAudioAnalysis_df = pd.read_csv(short_file_path + "knn_suggestions_according_sound_pandas_imp_pyAudioAnalysis.csv")
     pyAudioAnalysis_df['Method'] = 'pyAudioAnalysis'
     # add negative examples from vector representation methods
-    # turicrete mistakes have larger distance from other methods
-    turicreate_df = turicreate_df[turicreate_df['Distance'] >= 4]  # add distance to parameters?
+    # turicreate mistakes have larger distance from other methods
+    turicreate_df = turicreate_df[turicreate_df['Distance'] >= 4]
     dfs = [turicreate_df, wav2vec_df, pyAudioAnalysis_df]
     methods_df = pd.concat(dfs)
-    methods_df = methods_df[methods_df['Distance'] >= 1]  # add distance to parameters?
+    methods_df = methods_df[methods_df['Distance'] >= 1]
     methods_df = methods_df.rename(columns={'Candidate': 'Negative'})
     filtered_df = filtered_df.rename(columns={'Candidate': 'Positive',
                                               'Distance': 'Distance_Spoken_Name',
@@ -52,23 +54,10 @@ def second_phase_negative_examples(short_file_path, dataset, filtered_df):
     return unique_df
 
 
-# !!!!!!!!!!!!!!!! NEEDED? CHECK
-# CHECK IF NEEDED AFTER DUPLICATE REMOVE FUNCTION
-# # find unique pairs(original, positive)
-# originals = triplets_df['Original'].tolist()
-# positives = triplets_df['Positive'].tolist()
-# negatives = triplets_df['Negative'].tolist()
-
-# pairs = []
-# for i, org in enumerate(originals):
-#   pairs += [org + positives[i]]
-
-# pairs_df = pd.DataFrame({'Pairs': pairs, 'originals': originals,
-#                          'Positive': positives, 'Negative': negatives})
-
-# TODO: check if needed
 def add_random_negatives_phase_two(unique_df):
-    # create triplets of data ###################check if needed (easy negative examples)
+    """
+    The function creates triplets of data and adds easy negative examples
+    """
     originals = unique_df['Original'].tolist()
     positives = unique_df['Positive'].tolist()
     negatives = unique_df['Negative'].tolist()
@@ -79,18 +68,13 @@ def add_random_negatives_phase_two(unique_df):
         trios += [[originals[i], positives[i], random_negative]]
 
     random.shuffle(trios)
-
-    print(trios[0])
-    print(len(trios))
     return trios
 
 
 def train_test_split_phase_two(train_data, test_data, trios):
-    # l = int(0.66 * len(unique_df))
-    # train_data = unique_df[:l]
-    # test_data = unique_df[l:]
-    # test_data
-    ######################### DELETE IF NOT NEEDED TO ADD RANDOM NEGATIVES (use above)
+    """
+    The function splits the phase two data into train and test
+    """
     l = int(0.66 * len(trios))
     train_data += trios[:l]
     test_data += trios[l:]
